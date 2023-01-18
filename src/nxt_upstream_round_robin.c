4a424f9eaf7d4d705548a84475573092aca792e4
Catalin Stochita <cstochita@gradientcyber.com>
2023-01-18 04:22:47 +0200

    1

--- src/nxt_upstream_round_robin.c
+++ src/nxt_upstream_round_robin.c
@@ -166,14 +166,14 @@ static void *nxt_upstream_health_handler(void *arg)
         nxt_log(task, NXT_LOG_NOTICE, "CATA LOG REPET");
         for (i = 0; i < n; i++)
         {
-            if (urr->server[i].health_status == 1)
-            {
-                urr->server[i].health_status = 0;
-            }
-            else
-            {
-                urr->server[i].health_status = 1;
-            }
+            // if (urr->server[i].health_status == 1)
+            // {
+            //     urr->server[i].health_status = 0;
+            // }
+            // else
+            // {
+            //     urr->server[i].health_status = 1;
+            // }
             // nxt_http_request_send(task, r, out);
             // }
             sleep(30);
