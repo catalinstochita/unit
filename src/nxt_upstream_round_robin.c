d5cc8e1b294c96f4086f4f22504f5b51f92d240b
Catalin Stochita <cstochita@gradientcyber.com>
2023-01-18 03:14:45 +0200

    thread

--- src/nxt_upstream_round_robin.c
+++ src/nxt_upstream_round_robin.c
@@ -151,10 +151,10 @@ static void *nxt_upstream_health_handler(void *arg)
 {
     int i, n;
     struct arg_struct *args = (struct arg_struct *)arg;
-    nxt_task_t *task = args->task;
+    // nxt_task_t *task = args->task;
     nxt_upstream_round_robin_t *urr = args->urr;
-    struct nxt_http_request_s *r = malloc(sizeof(struct nxt_http_request_s));
-    struct nxt_buf_s *out = malloc(sizeof(struct nxt_buf_s));
+    // struct nxt_http_request_s *r = malloc(sizeof(struct nxt_http_request_s));
+    // struct nxt_buf_s *out = malloc(sizeof(struct nxt_buf_s));
     n = urr->items;
     while (1)
     {
