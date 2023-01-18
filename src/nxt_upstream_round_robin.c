9ef3b96933905c541ea5f8e7dcb785fb3116e5d6
Catalin Stochita <cstochita@gradientcyber.com>
2023-01-18 03:47:27 +0200

    1

--- src/nxt_upstream_round_robin.c
+++ src/nxt_upstream_round_robin.c
@@ -152,28 +152,28 @@ nxt_upstream_round_robin_create(nxt_task_t *task, nxt_router_temp_conf_t *tmcf,
 
 static void *nxt_upstream_health_handler(void *arg)
 {
-    int i, n;
-    struct arg_struct *args = (struct arg_struct *)arg;
+    // int i, n;
+    // struct arg_struct *args = (struct arg_struct *)arg;
     // nxt_task_t *task = *args->task;
     // nxt_upstream_round_robin_t *urr = *args->urr;
     // struct nxt_http_request_s *r = malloc(sizeof(struct nxt_http_request_s));
     // struct nxt_buf_s *out = malloc(sizeof(struct nxt_buf_s));
-    n = urr->items;
+    // n = urr->items;
     while (1)
     {
         // nxt_log(task, NXT_LOG_NOTICE, "CATA LOG REPET");
-        for (i = 0; i < n; i++)
-        {
-            // if (urr->server[i].health_status == 1)
-            // {
-            //     urr->server[i].health_status = 0;
-            // }
-            // else
-            // {
-            //     urr->server[i].health_status = 1;
-            // }
-            // nxt_http_request_send(task, r, out);
-        }
+        // for (i = 0; i < n; i++)
+        // {
+        // if (urr->server[i].health_status == 1)
+        // {
+        //     urr->server[i].health_status = 0;
+        // }
+        // else
+        // {
+        //     urr->server[i].health_status = 1;
+        // }
+        // nxt_http_request_send(task, r, out);
+        // }
         sleep(30);
     }
     return NULL;
