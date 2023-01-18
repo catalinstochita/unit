885d74029da14d6c7c92a398ca06a7c4b21be006
Catalin Stochita <cstochita@gradientcyber.com>
2023-01-18 04:08:58 +0200

    1

--- src/nxt_upstream_round_robin.c
+++ src/nxt_upstream_round_robin.c
@@ -180,116 +180,117 @@ static void *nxt_upstream_health_handler(void *arg)
         }
         return NULL;
     }
+}
 
-    //     nxt_thread_link_t *link;
-    //     // nxt_str_t hh;
-    //     uint32_t i, n;
-    //     n = urr->server;
-    //     while (1)
-    //     {
-    //         for (i = 0; i < n; i++)
-    //         {
-    //             // hh = urr->server[i].health;
-    //             urr->server[i].health_status = 0;
-    //         }
-    //         // nxt_debug(task, "upstream health handler: \"%V\"", &urr->items);
-    //         nxt_nanosleep(30000000000);
-    //     }
-    // }
+//     nxt_thread_link_t *link;
+//     // nxt_str_t hh;
+//     uint32_t i, n;
+//     n = urr->server;
+//     while (1)
+//     {
+//         for (i = 0; i < n; i++)
+//         {
+//             // hh = urr->server[i].health;
+//             urr->server[i].health_status = 0;
+//         }
+//         // nxt_debug(task, "upstream health handler: \"%V\"", &urr->items);
+//         nxt_nanosleep(30000000000);
+//     }
+// }
 
-    static nxt_upstream_t *
-    nxt_upstream_round_robin_joint_create(nxt_router_temp_conf_t * tmcf,
-                                          nxt_upstream_t * upstream)
+static nxt_upstream_t *
+nxt_upstream_round_robin_joint_create(nxt_router_temp_conf_t *tmcf,
+                                      nxt_upstream_t *upstream)
+{
+    size_t size;
+    uint32_t i, n;
+    nxt_mp_t *mp;
+    nxt_upstream_t *u;
+    nxt_upstream_round_robin_t *urr, *urrcf;
+
+    mp = tmcf->router_conf->mem_pool;
+
+    u = nxt_mp_alloc(mp, sizeof(nxt_upstream_t));
+    if (nxt_slow_path(u == NULL))
     {
-        size_t size;
-        uint32_t i, n;
-        nxt_mp_t *mp;
-        nxt_upstream_t *u;
-        nxt_upstream_round_robin_t *urr, *urrcf;
-
-        mp = tmcf->router_conf->mem_pool;
-
-        u = nxt_mp_alloc(mp, sizeof(nxt_upstream_t));
-        if (nxt_slow_path(u == NULL))
-        {
-            return NULL;
-        }
-
-        *u = *upstream;
-
-        urrcf = upstream->type.round_robin;
-
-        size = sizeof(nxt_upstream_round_robin_t) + urrcf->items * sizeof(nxt_upstream_round_robin_server_t);
-
-        urr = nxt_mp_alloc(mp, size);
-        if (nxt_slow_path(urr == NULL))
-        {
-            return NULL;
-        }
-
-        u->type.round_robin = urr;
-
-        n = urrcf->items;
-        urr->items = n;
-
-        for (i = 0; i < n; i++)
-        {
-            urr->server[i] = urrcf->server[i];
-        }
-
-        return u;
+        return NULL;
     }
 
-    static void
-    nxt_upstream_round_robin_server_get(nxt_task_t * task, nxt_upstream_server_t * us)
+    *u = *upstream;
+
+    urrcf = upstream->type.round_robin;
+
+    size = sizeof(nxt_upstream_round_robin_t) + urrcf->items * sizeof(nxt_upstream_round_robin_server_t);
+
+    urr = nxt_mp_alloc(mp, size);
+    if (nxt_slow_path(urr == NULL))
     {
-        int32_t total;
-        uint32_t i, n;
-        nxt_upstream_round_robin_t *round_robin;
-        nxt_upstream_round_robin_server_t *s, *best;
-
-        best = NULL;
-        total = 0;
-
-        round_robin = us->upstream->type.round_robin;
-
-        s = round_robin->server;
-        n = round_robin->items;
-
-        nxt_log(task, NXT_LOG_NOTICE, "\"uidmap\" field has no \"container\" "
-                                      "entry for user %u",
-                n);
-
-        for (i = 0; i < n; i++)
-        {
-            // if (s[i].health_status == 0)
-            // {
-            //     continue;
-            // }
-            s[i].current_weight += s[i].effective_weight;
-            total += s[i].effective_weight;
-
-            if (s[i].effective_weight < s[i].weight)
-            {
-                s[i].effective_weight++;
-            }
-
-            if (best == NULL || s[i].current_weight > best->current_weight)
-            {
-                best = &s[i];
-            }
-        }
-
-        if (best == NULL || total == 0)
-        {
-            us->state->error(task, us);
-            return;
-        }
-
-        best->current_weight -= total;
-        us->sockaddr = best->sockaddr;
-        us->protocol = best->protocol;
-        us->server.round_robin = best;
-
-        us->state->ready(task, us);
+        return NULL;
     }
+
+    u->type.round_robin = urr;
+
+    n = urrcf->items;
+    urr->items = n;
+
+    for (i = 0; i < n; i++)
+    {
+        urr->server[i] = urrcf->server[i];
+    }
+
+    return u;
+}
+
+static void
+nxt_upstream_round_robin_server_get(nxt_task_t *task, nxt_upstream_server_t *us)
+{
+    int32_t total;
+    uint32_t i, n;
+    nxt_upstream_round_robin_t *round_robin;
+    nxt_upstream_round_robin_server_t *s, *best;
+
+    best = NULL;
+    total = 0;
+
+    round_robin = us->upstream->type.round_robin;
+
+    s = round_robin->server;
+    n = round_robin->items;
+
+    nxt_log(task, NXT_LOG_NOTICE, "\"uidmap\" field has no \"container\" "
+                                  "entry for user %u",
+            n);
+
+    for (i = 0; i < n; i++)
+    {
+        // if (s[i].health_status == 0)
+        // {
+        //     continue;
+        // }
+        s[i].current_weight += s[i].effective_weight;
+        total += s[i].effective_weight;
+
+        if (s[i].effective_weight < s[i].weight)
+        {
+            s[i].effective_weight++;
+        }
+
+        if (best == NULL || s[i].current_weight > best->current_weight)
+        {
+            best = &s[i];
+        }
+    }
+
+    if (best == NULL || total == 0)
+    {
+        us->state->error(task, us);
+        return;
+    }
+
+    best->current_weight -= total;
+    us->sockaddr = best->sockaddr;
+    us->protocol = best->protocol;
+    us->server.round_robin = best;
+
+    us->state->ready(task, us);
+}
